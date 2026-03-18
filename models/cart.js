0d23a5f491a4fb3916b5bda3a3cd4ad9bde77f2d
zieadkh <zyadk3503@gmail.com>
2026-03-18 13:17:32 +0200

    Implement product deletion functionality and fix cart data structure

--- models/cart.js
+++ models/cart.js
@@ -11,7 +11,7 @@ const p = path.join(
 module.exports = class Cart {
   static addProduct(id, productPrice) {
     fs.readFile(p, (err, fileContent) => {
-      let cart = { products: [], totlPrice: 0 };
+      let cart = { products: [], totalPrice: 0 };
       if (!err) {
         cart = JSON.parse(fileContent);
       }
@@ -27,10 +27,27 @@ module.exports = class Cart {
         updatedProduct = { id: id, qty: 1 };
         cart.products = [...cart.products, updatedProduct];
       }
-      cart.totlPrice = cart.totlPrice + +productPrice;
+      cart.totalPrice = cart.totalPrice + +productPrice;
       fs.writeFile(p, JSON.stringify(cart), err => {
         console.log(err);
       });
     });
   }
-}
+
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find(prod => prod.id === id);
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+      updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      });
+    });
+  }
+};
